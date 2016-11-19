a73219e0c1d168dd3cd1da71ee7d970e4a6fc412
ls <ls_andro@163.com>
2016-11-19 15:30:13 +0800

    添加手机号校验

--- utils/StringUtils.java
+++ utils/StringUtils.java
@@ -266,4 +266,20 @@ public class StringUtils {
 		return false;
 
 	}
+	
+	/**
+     * 判断是否是手机号
+     * @param phone
+     * @return
+     */
+	public static boolean checkPhone(String phone) {  
+        Pattern pattern = Pattern  
+                .compile("^(13[0-9]|15[0-9]|153|15[6-9]|180|18[23]|18[5-9])\\d{8}$");  
+        Matcher matcher = pattern.matcher(phone);  
+  
+        if (matcher.matches()) {  
+            return true;  
+        }  
+        return false;  
+    }
 }
